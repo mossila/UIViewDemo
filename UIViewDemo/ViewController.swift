6abaff8d1061b7a0760fb420fbda45d71d53dc3a
Sutean Rutjanalard <sutean@siamsquared.com>
2016-12-09 21:42:04 +0700

    Initial Commit

---
+++ UIViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIViewDemo
+//
+//  Created by Sutean Rutjanalard on 12/9/2559 BE.
+//  Copyright © 2559 Sutean Rutjanalard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
